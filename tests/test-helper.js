3c483de83062e7ddf302db2c485ad06cce3e6c32
Cyril Fluck <cyril.fluck@addepar.com>
2018-04-23 17:15:58 -0700

    Install the RAF waiter to remove test flakiness

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -3,6 +3,8 @@ import {
   setResolver
 } from 'ember-qunit';
 import { start } from 'ember-cli-qunit';
+import registerRAFWaiter from 'ember-raf-scheduler/test-support/register-waiter';
 
+registerRAFWaiter();
 setResolver(resolver);
 start();

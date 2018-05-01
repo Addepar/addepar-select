f3f2b23fc9fe57f43cfe1df22a11a1f1cc4401d6
Cyril Fluck <cyril.fluck@addepar.com>
2018-05-01 11:58:49 -0700

    Fix Ember 1.11 by importing initializer for vertical-collection

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -4,6 +4,12 @@ import {
 } from 'ember-qunit';
 import { start } from 'ember-cli-qunit';
 import registerRAFWaiter from 'ember-raf-scheduler/test-support/register-waiter';
+import { GTE_EMBER_1_13 } from 'ember-compatibility-helpers';
+
+if (!GTE_EMBER_1_13) {
+  let initializer = window.require('dummy/initializers/vertical-collection-legacy-compat').default;
+  initializer.initialize();
+}
 
 registerRAFWaiter();
 setResolver(resolver);
